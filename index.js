18fa85fcb6bc1eb27dc45c4fd0e48e2bd1e9d443
amishagarg <amishagarg902@gmail.com>
2021-02-08 18:17:23 +0530

    DOM1

---
+++ index.js
@@ -0,0 +1,11 @@
+document.querySelector("h1").innerHTML = "Good Bye";
+
+//document.querySelector("h1").classList.add("huge");   ----> to add the class huge to <h1>
+
+//document.querySelector("h1").textConent;  ----> show the text content of <h1>
+
+//document.querySelector("a").attributes;  ---->will show all the attributes under <a> eg. href
+
+//document.querySelector("a").getAttribute("href");  ----> whill show what href attribute contains
+
+//document.querySelector("a").setAttribute("href", "https://www.bing.com/"); ----> will set attribute of old href(google.com) to new href(bing.com).
